0c47ffbc6d7686278980e32dc4137415350cba2f
Roopesh Reddy <cr.reddy@hotmail.com>
2014-06-18 13:49:26 +0530

    Added demo.htm page to test the WebUtils

--- WebUtils.js
+++ WebUtils.js
@@ -3,16 +3,6 @@ var WU = {
     page: {}
 };
 
-WU.input.onlyNumerics = function (evt) {
-    "use strict";
-    var charCode = evt.keyCode || evt.which;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
-    }
-    return true;
-};
-
-
 WU.input.numericTextBox = function (evt) {
     "use strict";
     var charCode = evt.keyCode;
@@ -20,36 +10,32 @@ WU.input.numericTextBox = function (evt) {
             charCode === 46 || //delete
             charCode === 13) { //enter key
 
-        return true;
+        return;
     } else if (charCode >= 37 && charCode <= 40) { //arrow keys
 
-        return true;
+        return;
     } else if (charCode >= 48 && charCode <= 57) { //0-9 on key pad
 
         if (evt.shiftKey || evt.ctrlKey) {
-            return false;
+            evt.preventDefault();
         }
 
-        return true;
     } else if (charCode >= 96 && charCode <= 105) { //0-9 on num pad
     
         if (evt.shiftKey || evt.ctrlKey) {
-            return false;
+            evt.preventDefault();
         }
 
-        return true;
     } else {
-        return false;
+        return evt.preventDefault();
     }
 };
 
-WU.input.allowableLength = function (ctrl, size) {
+WU.input.allowableLength = function (ctrl, size, event) {
     "use strict";
     if (ctrl !== null && size !== null) {
         if (ctrl.value.length > size) {
-            return false;
-        } else {
-            return true;
+            event.preventDefault();
         }
     }
 };
